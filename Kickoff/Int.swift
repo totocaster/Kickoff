68f6b0bd6939dfcea3ccd6d2cbb1ffdea92eb5bc
Toto Tvalavadze <totocaster@me.com>
2018-02-03 10:58:47 +0900

    Add isOdd and isEven

--- Kickoff/Int.swift
+++ Kickoff/Int.swift
@@ -2,7 +2,7 @@ import Foundation
 
 public extension Int {
 
-    // MARK: Random numbers for Int
+    // MARK: Random numbers
 
     public init(randomIn range: Range<Int>) {
         self.init(randomFrom: range.lowerBound, to: range.upperBound - 1)
@@ -15,4 +15,13 @@ public extension Int {
     public init(randomFrom lower: Int = 0, to upper: Int = 100){
         self.init(lower + Int(arc4random_uniform(UInt32(upper - lower + 1))))
     }
+
+    // MARK: Lazy odd/even checks
+
+    public var isEven: Bool {
+        return (self % 2 == 0)
+    }
+
+    public var isOdd: Bool { return !isEven }
+
 }
