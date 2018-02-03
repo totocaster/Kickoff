8a860497296d8b76fa4ec9717c7ce4401767f04c
Toto Tvalavadze <totocaster@me.com>
2018-02-03 10:36:45 +0900

    Add docs to `Bundle` extensions

--- Kickoff/Bundle.swift
+++ Kickoff/Bundle.swift
@@ -1,23 +1,31 @@
 import Foundation
 
 public extension Bundle {
+
+    /// Returns string for `CFBundleIdentifier` key.
     public var identifier: String {
         return self.infoDictionary?["CFBundleIdentifier"] as? String ?? "Unknown"
     }
 
-    public var shortVersionString: String {
+    /// Returns string for `CFBundleShortVersionString` key.
+    public var version: String {
         return self.infoDictionary?["CFBundleShortVersionString"] as? String ?? "Unknown"
     }
 
-    public var buildNumberString: String {
+    /// Returns string for `CFBundleVersion` key.
+    public var build: String {
         return self.infoDictionary?["CFBundleVersion"] as? String ?? "0"
     }
 
+    /// Returns formatted string of app version followerd by build number in prentices.
+    /// For example `1.3.7 (1273)`.
     public var formattedVersion: String {
-        return "\(shortVersionString) (\(buildNumberString))"
+        return "\(version) (\(build))"
     }
 
+    /// Returns string for `CFBundleDisplayName` key.
+    /// Usually app name that appears on the home screen.
     public var displayName: String {
-        return self.infoDictionary?["CFBundleDisplayName"] as? String ?? "Project Anvil"
+        return self.infoDictionary?["CFBundleDisplayName"] as? String ?? "Unknown"
     }
 }
