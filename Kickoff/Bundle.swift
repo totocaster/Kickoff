e29c3ad8be3c55be8d7a887021b8b3effe439aa5
Toto Tvalavadze <totocaster@me.com>
2018-02-03 11:11:07 +0900

    Add environment checks to Bundle

--- Kickoff/Bundle.swift
+++ Kickoff/Bundle.swift
@@ -28,4 +28,28 @@ public extension Bundle {
     public var displayName: String {
         return self.infoDictionary?["CFBundleDisplayName"] as? String ?? "Unknown"
     }
+
+    // MARK: - Enviromental Checks
+
+    public static var isDebug: Bool {
+        #if DEBUG
+            return true
+        #else
+            return false
+        #endif
+    }
+
+    public static var isSimulator: Bool {
+        #if (arch(i386) || arch(x86_64)) // some strong assumptions here.
+            return true
+        #else
+            return false
+        #endif
+    }
+
+    /// http://stackoverflow.com/questions/12431994/detect-testflight
+    // TODO: Review https://stackoverflow.com/questions/9906504/check-if-ios-app-is-live-in-app-store
+    public static var isTestFlight: Bool {
+        return Bundle.main.appStoreReceiptURL?.path.contains("sandboxReceipt") == true
+    }
 }
