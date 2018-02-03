ef49e78df8460edce04440883da6c1486b6c7e33
Toto Tvalavadze <totocaster@me.com>
2018-02-03 12:50:27 +0900

    Add warchOS and tcOS targets

---
+++ Kickoff Watch/Kickoff_Watch.h
@@ -0,0 +1,19 @@
+//
+//  Kickoff_Watch.h
+//  Kickoff Watch
+//
+//  Created by Toto Tvalavadze on 2018/02/03.
+//  Copyright © 2018 Toto Tvalavadze. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for Kickoff_Watch.
+FOUNDATION_EXPORT double Kickoff_WatchVersionNumber;
+
+//! Project version string for Kickoff_Watch.
+FOUNDATION_EXPORT const unsigned char Kickoff_WatchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Kickoff_Watch/PublicHeader.h>
+
+
---
+++ Kickoff tvOS/Kickoff_tvOS.h
@@ -0,0 +1,19 @@
+//
+//  Kickoff_tvOS.h
+//  Kickoff tvOS
+//
+//  Created by Toto Tvalavadze on 2018/02/03.
+//  Copyright © 2018 Toto Tvalavadze. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Kickoff_tvOS.
+FOUNDATION_EXPORT double Kickoff_tvOSVersionNumber;
+
+//! Project version string for Kickoff_tvOS.
+FOUNDATION_EXPORT const unsigned char Kickoff_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Kickoff_tvOS/PublicHeader.h>
+
+
