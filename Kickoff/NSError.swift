5320f106a55c9b728c8619da203125e0d433b092
Toto Tvalavadze <totocaster@me.com>
2018-02-03 10:41:41 +0900

    Add docs to NSError

--- Kickoff/NSError.swift
+++ Kickoff/NSError.swift
@@ -1,9 +1,17 @@
 import Foundation
 
 public extension NSError {
-    public convenience init(localizedDescription: String) {
+
+
+    /// Conveniance initializer for quiqly creating erros in given domain with friendly error messaegs.
+    /// Error message is assigned to error's `NSLocalizedDescriptionKey`.
+    ///
+    /// - Parameters:
+    ///   - localizedDescription: Human friently error message.
+    ///   - domain: Optional domain where error happened. By default matches your bundle identifier.
+    public convenience init(localizedDescription: String, in domain: String = Bundle.main.identifier) {
         self.init(
-            domain: Bundle.main.identifier,
+            domain: domain,
             code: 100,
             userInfo: [NSLocalizedDescriptionKey : localizedDescription])
     }
