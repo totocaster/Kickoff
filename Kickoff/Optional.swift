f96adc365ce39f8c4db6fe0be39b330b34f70f8a
Toto Tvalavadze <totocaster@me.com>
2018-09-01 15:36:29 +0900

    Add Optional sugar from appventure.me
    Ref: http://appventure.me/2018/01/10/optional-extensions/

--- Kickoff/Optional.swift
+++ Kickoff/Optional.swift
@@ -12,3 +12,73 @@ public extension Optional where Wrapped: Collection {
         return self?.isEmpty ?? true
     }
 }
+
+// Emptiness
+public extension Optional {
+    /// Returns true if the optional is empty
+    var isNone: Bool {
+        return self == .none
+    }
+    
+    /// Returns true if the optional is not empty
+    var isSome: Bool {
+        return self != .none
+    }
+}
+
+// Or
+public extension Optional {
+    /// Return the value of the Optional or the `default` parameter
+    /// - param: The value to return if the optional is empty
+    func or(_ default: Wrapped) -> Wrapped {
+        return self ?? `default`
+    }
+    
+    /// Returns the unwrapped value of the optional *or*
+    /// the result of an expression `else`
+    /// I.e. optional.or(else: print("Arrr"))
+    func or(else: @autoclosure () -> Wrapped) -> Wrapped {
+        return self ?? `else`()
+    }
+    
+    /// Returns the unwrapped value of the optional *or*
+    /// the result of calling the closure `else`
+    /// I.e. optional.or(else: {
+    /// ... do a lot of stuff
+    /// })
+    func or(else: () -> Wrapped) -> Wrapped {
+        return self ?? `else`()
+    }
+    
+    /// Returns the unwrapped contents of the optional if it is not empty
+    /// If it is empty, throws exception `throw`
+    func or(throw exception: Error) throws -> Wrapped {
+        guard let unwrapped = self else { throw exception }
+        return unwrapped
+    }
+}
+
+extension Optional where Wrapped == Error {
+    /// Only perform `else` if the optional has a non-empty error value
+    func or(_ else: (Error) -> Void) {
+        guard let error = self else { return }
+        `else`(error)
+    }
+}
+// Filter and expect
+extension Optional {
+    /// Returns the unwrapped value of the optional only if
+    /// - The optional has a value
+    /// - The value satisfies the predicate `predicate`
+    func filter(_ predicate: (Wrapped) -> Bool) -> Wrapped? {
+        guard let unwrapped = self,
+            predicate(unwrapped) else { return nil }
+        return self
+    }
+    
+    /// Returns the wrapped value or crashes with `fatalError(message)`
+    func expect(_ message: String) -> Wrapped {
+        guard let value = self else { fatalError(message) }
+        return value
+    }
+}
